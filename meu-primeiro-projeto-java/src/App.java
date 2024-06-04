e31f74518de8702e6fd9b720a373a85fdcb2b664
iamDiegoOliveira <diegods1505@gmail.com>
2024-06-04 10:54:37 -0300

    criando o projeto para exemplo de tipos variaveis

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,20 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        String nome = "José";
+        String sobrenome = "Diego";
+        String nomeCompleto = nomeCompleto(nome, sobrenome);
+
+        System.out.println(nomeCompleto);
+       
+       }
+
+
+       public static String nomeCompleto (String nome, String sobrenome){
+
+        return nome.concat(" ").concat(sobrenome);
+   
+
+       }
+
     }
-}
