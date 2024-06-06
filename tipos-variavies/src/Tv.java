60c065b6b9d00c3a8dcce3c987b00cf2dcfffd18
iamDiegoOliveira <diegods1505@gmail.com>
2024-06-06 10:20:51 -0300

    Testando metodos

---
+++ tipos-variavies/src/Tv.java
@@ -0,0 +1,20 @@
+public class Tv {
+
+    private boolean ligada;
+    private int canal;
+    private int volume;
+
+
+
+
+    public boolean ligar(boolean ligada){
+        if (ligada == true) {
+            System.out.println("A Tv está ligada.");
+        }
+        return ligada;
+
+    }
+
+  
+
+}
---
+++ tipos-variavies/src/app.java
@@ -0,0 +1,13 @@
+public class app {
+    public static void main(String[] args) throws Exception {
+        
+
+        Tv smart = new Tv();
+        smart.ligar(true);
+        
+
+
+
+
+    }
+}
